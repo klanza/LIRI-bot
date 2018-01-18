cc2e64c7af7eea755231397cf7f647134d0e6952
klanza <klanza91@gmail.com>
2018-01-17 22:29:58 -0700

    Finished all basic functions for LIRI program. Working on logging responses to text file

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ const keys = require('./keys.js')
 const Twitter = require('twitter')
 const Spotify = require('node-spotify-api')
 const request = require('request')
+const fs = require('fs')
 
 // Generating user information using twitter API keys
 const twitterUser = new Twitter(keys.twitterKeys)
@@ -13,96 +14,117 @@ const twitterUser = new Twitter(keys.twitterKeys)
 let spotifyUser = new Spotify(keys.spotifyKeys)
 
 // Constants for command
-const command = process.argv[2]
-const userQuery = process.argv[3]
+let command = process.argv[2]
+let userQuery = process.argv[3]
+const nodeCommands = function(command, userQuery) {
+  if (command === 'my-tweets') {
+    const params = {screen_name: 'KennethLanza91'}
+    twitterUser.get('statuses/user_timeline', params, function (error, tweets, response) {
+      if (!error) {
+        console.log(tweets.length)
+        tweets.forEach(element => {
+          // console.log('===========')
+          console.log(element.created_at)
+          console.log(element.text)
+          console.log('===================================================')
+        })
+      }
+    })
+  }
 
-if (command === 'my-tweets') {
-  const params = {screen_name: 'KennethLanza91'}
-  twitterUser.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-      console.log(tweets.length)
-      tweets.forEach(element => {
-        // console.log('===========')
-        console.log(element.created_at)
-        console.log(element.text)
+  if (command === 'spotify-this-song' && userQuery === undefined) {
+    console.log('---------------------------------------------------')
+    console.log('Please include the song name, in quotations, after the Spotify command to use this function.')
+    console.log('Here is an example response:')
+    spotifyUser
+      .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+      .then(function (data) {
+        console.log('===================================================')
+        console.log('Artist: ' + data.artists[0].name)
+        console.log('Song: ' + data.name)
+        console.log('Album: ' + data.album.name)
+        console.log('Preview: ' + data.preview_url)
         console.log('===================================================')
       })
-    }
-  })
-}
+      .catch(function (err) {
+        console.error('Error occurred: ' + err)
+      })
+  }
 
-if (command === 'spotify-this-song' && userQuery === undefined) {
-  console.log('---------------------------------------------------')
-  console.log('Please include the song name, in quotations, after the Spotify command to use this function.')
-  console.log('Here is an example response:')
-  spotifyUser
-    .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
-    .then(function (data) {
+  if (command === 'spotify-this-song' && typeof (userQuery) === 'string') {
+    spotifyUser.search({ type: 'track', query: userQuery, limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err)
+      }
       console.log('===================================================')
-      console.log('Artist: ' + data.artists[0].name)
-      console.log('Song: ' + data.name)
-      console.log('Album: ' + data.album.name)
-      console.log('Preview: ' + data.preview_url)
+      console.log('Artist: ' + data.tracks.items[0].artists[0].name)
+      console.log('Song: ' + data.tracks.items[0].name)
+      console.log('Preview: ' + data.tracks.items[0].preview_url)
+      console.log('Album: ' + data.tracks.items[0].album.name)
       console.log('===================================================')
     })
-    .catch(function (err) {
-      console.error('Error occurred: ' + err)
+  }
+
+  if (command === 'movie-this' && userQuery === undefined) {
+    request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=40e9cece', function (error, response, body) {
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode === 200) {
+        console.log('---------------------------------------------------')
+        console.log('Please include the movie title, in quotations, after the Spotify command to use this function.')
+        console.log('Here is an example response:')
+        console.log('===================================================')
+        console.log('Title: ' + JSON.parse(body).Title)
+        console.log('Year: ' + JSON.parse(body).Year)
+        console.log('IMDB rating: ' + JSON.parse(body).Ratings[0].Value)
+        console.log('Rotten Tomatoes rating: ' + JSON.parse(body).Ratings[1].Value)
+        console.log('Production country: ' + JSON.parse(body).Country)
+        console.log('Language: ' + JSON.parse(body).Title)
+        console.log('Plot: ' + JSON.parse(body).Plot)
+        console.log('Actors: ' + JSON.parse(body).Actors)
+        console.log('===================================================')
+      }
     })
+  }
+
+  if (command === 'movie-this' && typeof (userQuery) === 'string') {
+    request(`http://www.omdbapi.com/?t=${userQuery}&y=&plot=short&apikey=40e9cece`, function (error, response, body) {
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode === 200) {
+        const movieResponse = [
+          '===================================================',
+          'Title: ' + JSON.parse(body).Title,
+          'Year: ' + JSON.parse(body).Year,
+          'IMDB rating: ' + JSON.parse(body).Ratings[0].Value,
+          'Rotten Tomatoes rating: ' + JSON.parse(body).Ratings[1].Value,
+          'Production country: ' + JSON.parse(body).Country,
+          'Language: ' + JSON.parse(body).Title,
+          'Plot: ' + JSON.parse(body).Plot,
+          'Actors: ' + JSON.parse(body).Actors,
+          '==================================================='
+        ]
+        movieResponse.forEach(function (element) {
+          console.log(element)
+        })
+        const movieResponseLog = movieResponse.join('\r\n')
+        fs.appendFile('log.txt', '\n' + movieResponseLog, (err) => {
+          if (err) throw err
+          console.log('The "data to append" was appended to file!')
+        })
+      }
+    })
+  }
+
+  if (command === 'do-what-it-says') {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+      if (error) {
+        return console.log(error)
+      }
+      const input = data.split(',')
+      let command = input[0]
+      let userQuery = input[1]
+      nodeCommands(command, userQuery)
+    })
+  }
 }
 
-if (command === 'spotify-this-song' && typeof (userQuery) === 'string') {
-  spotifyUser.search({ type: 'track', query: userQuery, limit: 1 }, function (err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err)
-    }
-    console.log('===================================================')
-    console.log('Artist: ' + data.tracks.items[0].artists[0].name)
-    console.log('Song: ' + data.tracks.items[0].name)
-    console.log('Preview: ' + data.tracks.items[0].preview_url)
-    console.log('Album: ' + data.tracks.items[0].album.name)
-    console.log('===================================================')
-  })
-}
-
-if (command === 'movie-this' && userQuery === undefined) {
-  request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=40e9cece', function (error, response, body) {
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
-      console.log('---------------------------------------------------')
-      console.log('Please include the movie title, in quotations, after the Spotify command to use this function.')
-      console.log('Here is an example response:')
-      console.log('===================================================')
-      console.log('Title: ' + JSON.parse(body).Title)
-      console.log('Year: ' + JSON.parse(body).Year)
-      console.log('IMDB rating: ' + JSON.parse(body).Ratings[0].Value)
-      console.log('Rotten Tomatoes rating: ' + JSON.parse(body).Ratings[1].Value)
-      console.log('Production country: ' + JSON.parse(body).Country)
-      console.log('Language: ' + JSON.parse(body).Title)
-      console.log('Plot: ' + JSON.parse(body).Plot)
-      console.log('Actors: ' + JSON.parse(body).Actors)
-      console.log('===================================================')
-    }
-  })
-}
-
-if (command === 'movie-this' && typeof (userQuery) === 'string') {
-  request(`http://www.omdbapi.com/?t=${userQuery}&y=&plot=short&apikey=40e9cece`, function (error, response, body) {
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
-      console.log('===================================================')
-      console.log('Title: ' + JSON.parse(body).Title)
-      console.log('Year: ' + JSON.parse(body).Year)
-      console.log('IMDB rating: ' + JSON.parse(body).Ratings[0].Value)
-      console.log('Rotten Tomatoes rating: ' + JSON.parse(body).Ratings[1].Value)
-      console.log('Production country: ' + JSON.parse(body).Country)
-      console.log('Language: ' + JSON.parse(body).Title)
-      console.log('Plot: ' + JSON.parse(body).Plot)
-      console.log('Actors: ' + JSON.parse(body).Actors)
-      console.log('===================================================')
-    }
-  })
-}
-
-if (command === 'do-what-it-says') {
-
-}
+nodeCommands(command, userQuery)
